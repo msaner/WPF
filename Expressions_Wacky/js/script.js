6cdd893fe9f00fdc62e8d79fe8e691886722e168
msaner <msaner@fullsail.edu>
2013-10-10 21:31:16 -0700

    starting fresh because other tries were doing nothing.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -8,11 +8,3 @@
 
 //set up a prompt to ask user to choose a number for the first part of their pirate name
 
-
-
-
-var firstName = prompt("To generate your First Name please choose a number between 0 & 3");
-
-var pirate1 = ["Captain", "Scallywag", "Tiny", "Moppet"]; //0, 1, 2, 3
-
-console.log(pirate1[firstname]);
