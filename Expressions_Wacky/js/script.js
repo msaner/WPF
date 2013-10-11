b4b3e06eeea959516355487eb03736b702d803b6
msaner <msaner@fullsail.edu>
2013-10-10 21:42:52 -0700

    added comments to the code and began second array for middle name

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -4,11 +4,17 @@
 // 10-10-2013
 // Expressions Wacky Assignment
 
-//Get your official Pirate name
+
 
 //set up a prompt to ask user to choose a number for the first part of their pirate name
 
+//lets set up an array that will contain all the possible first names for the Pirate name
  var pirate1 = ["Captain", "Tiny", "Scallywag", "Moppet"];
+ 
+ //now lets initiate a user prompt asking the user to choose a number that is linked to the index number of our array items
  var firstname = prompt("Generate your first name by choosing a number between 0 and 3.");
+ 
+ //rinse and repeat.  we will create another array to generate the middle name for our pirate.
+ var pirate2 = [""];
  
  console.log(pirate1[firstname]);
