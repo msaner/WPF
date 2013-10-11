cf44bc8fb4dbfb043d2aa1c1c70883c289dfbafe
msaner <msaner@fullsail.edu>
2013-10-10 21:14:35 -0700

    tried setting a prompt to choose a variable name from an array.  didn't work so deleted the prompt for now

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -7,6 +7,12 @@
 //Get your official Pirate name
 
 //set up a prompt to ask user to choose a number for the first part of their pirate name
+
+
+var piratefirst = prompt("To get the first part of your Pirate name choose a number between 0 and 3");
 var piratefirst = ["Captain", "Scallywag", "Peg Leg", "Moppet"] //0, 1, 2, 3
 
-var firstName = prompt("To get the first part of your Pirate name choose a number between 0 and 3");
+
+piratefirst[0] = Captain;
+
+console.log(piratefirst);
