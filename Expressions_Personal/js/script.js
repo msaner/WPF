de61e7bca79e356e846986b77a0e22087777c047
msaner <msaner@fullsail.edu>
2013-10-10 19:40:20 -0700

    added variable for days worked and used arithmetic operator to figure number of hours worked in a week.  tested via console output

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -6,9 +6,17 @@
 
 //Calculate how much money will  be earned in a week before taxes
 
-var hrScheduled = 9;
-var lunchBrk = 1;
+var hrScheduled = 9; //the number of hours scheduled to work each day
+var lunchBrk = 1; //non paid hours because of lunch break
 
-var actual = hrScheduled - lunchBrk;
+//to find actual number of paid hours worked in a day subtract the hrsScheduled from the lunchBrk and store this in var actual
+var actual = hrScheduled - lunchBrk; //actual is the actual number of hours that a person will be paid for in a day
 
-console.log(actual);
+//now calculate how many paid hours are worked in a week by multiplying the actual daily hours by the number of days in a week (daysWorked) and store in var wkHours
+var daysWorked = 5; //sets how many days a person works in a week
+
+var wkHours = actual * daysWorked;
+
+
+
+console.log(wkHours);
