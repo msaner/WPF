7efb88606b6d7159174547e6ee511bdeb9af6c60
msaner <msaner@fullsail.edu>
2013-10-10 18:56:49 -0700

    added var, const and var for the calculation output

--- Expressions_worksheet/js/dog-years.js
+++ Expressions_worksheet/js/dog-years.js
@@ -8,5 +8,10 @@
 // Calculate the age of a dog in dog years
 
 
-var age = 1*7;
-console.log(age)
+var actualAge = 1; //Sparky the dog is one year old
+const dogYears = 7; //Dogs age 7 times faster than humans so 1 human year is 7 dog years
+
+//to figure out how old Sparky is in dog years we have to multiple his age by 7 and assign that to dogAge
+var dogAge = actualAge * dogYears
+ 
+console.log(dogAge);
