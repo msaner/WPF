70337111a3cbed191fb46241274ba689f4338100
msaner <msaner@fullsail.edu>
2013-10-10 21:04:33 -0700

    first prompt added to generate first name

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -4,4 +4,7 @@
 // 10-10-2013
 // Expressions Wacky Assignment
 
-//
+//Get your official Pirate name
+
+//set up a prompt to ask user to choose a number for the first part of their pirate name
+var firstName = prompt("To get the first part of your Pirate name choose a number between 0 and 3");
