2a6dd484ec6feedda9d592c478cd941617605fce
msaner <msaner@fullsail.edu>
2013-10-10 21:09:06 -0700

    addisnged a variable to list out array that will store pirate first names

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -7,4 +7,6 @@
 //Get your official Pirate name
 
 //set up a prompt to ask user to choose a number for the first part of their pirate name
+var piratefirst = ["Captain", "Scallywag", "Peg Leg", "Moppet"] //0, 1, 2, 3
+
 var firstName = prompt("To get the first part of your Pirate name choose a number between 0 and 3");
