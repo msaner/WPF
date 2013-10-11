e212451d861abbd94a312dfa0393ed1621a42b9a
msaner <msaner@fullsail.edu>
2013-10-10 19:30:40 -0700

    put script.js into js folder

---
+++ Expressions_Personal/js/script.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+
+// Michael Saner
+// 10-10-2013
+// Expressions Personal Assignment
+
+//Calculate how much money will  be earned in a week before taxes
+
+var hrScheduled = 9;
+var lunchBrk = 1;
+
+var actual = hrScheduled - lunchBrk;
+
+console.log(actual);
