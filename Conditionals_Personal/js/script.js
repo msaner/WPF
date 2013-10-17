e413ee59414e76c7120492db88fde33417bda931
msaner <msaner@fullsail.edu>
2013-10-17 01:53:47 -0700

    added comment explaining the first if statement

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -9,6 +9,7 @@ var watts = prompt("How many watts is your microwave? (Normally it's either 700 
 //user input number of hot pockets they want to cook and stores value
 var hotPockets = prompt("How many Hot Pockets are you doing to cook?");
 
+//if a value was not given for either one of the prompts a message will be displayed to user
 if (watts === "" || hotPockets === ""){
 	console.log("You did not enter a number for one of the questions. Please try again.");
 }
