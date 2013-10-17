7fa2ada9967169fc0fd19e2c38f4d0068a8c1038
msaner <msaner@fullsail.edu>
2013-10-17 01:42:37 -0700

    created comment for hotPocket variable

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -6,4 +6,5 @@
 
 //user input the wattage of their microwave and stores value
 var watts = prompt("How many watts is your microwave? (Normally it's either 700 or 1100)");
+//user input number of hot pockets they want to cook and stores value
 var hotPockets = prompt("How many Hot Pockets are you doing to cook?");
