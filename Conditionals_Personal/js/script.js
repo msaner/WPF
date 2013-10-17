5da1aa5775184ae6a89fe8025a45a706c8169c44
msaner <msaner@fullsail.edu>
2013-10-17 01:55:07 -0700

    added else if   …if both prompts contain a value then code will continue to next step

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -12,4 +12,6 @@ var hotPockets = prompt("How many Hot Pockets are you doing to cook?");
 //if a value was not given for either one of the prompts a message will be displayed to user
 if (watts === "" || hotPockets === ""){
 	console.log("You did not enter a number for one of the questions. Please try again.");
-}
+}else if(watts >= 700){
+		
+	}
