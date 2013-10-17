58febcd4ba805fa51011f79477f506e07c863c45
msaner <msaner@fullsail.edu>
2013-10-17 02:27:44 -0700

    corrected a misspelled a word in the second prompt… doing supposed to be going

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -7,7 +7,7 @@
 //user input the wattage of their microwave and stores value
 var watts = prompt("How many watts is your microwave? (Normally it's either 700 or 1100)");
 //user input number of hot pockets they want to cook and stores value
-var hotPockets = prompt("How many Hot Pockets are you doing to cook?");
+var hotPockets = prompt("How many Hot Pockets are you going to cook?");
 
 //if a value was not given for either one of the prompts a message will be displayed to user
 if (watts === "" || hotPockets === ""){
