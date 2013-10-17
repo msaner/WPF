77c79f986d91b5d72ea8c51b99d0afa23b4fdb37
msaner <msaner@fullsail.edu>
2013-10-17 01:51:48 -0700

    wrote condition to check that a value was entered for both prompts.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -8,3 +8,7 @@
 var watts = prompt("How many watts is your microwave? (Normally it's either 700 or 1100)");
 //user input number of hot pockets they want to cook and stores value
 var hotPockets = prompt("How many Hot Pockets are you doing to cook?");
+
+if (watts === "" || hotPockets === ""){
+	console.log("You did not enter a number for one of the questions. Please try again.");
+}
