29847144c0e84cefdb25ed6206120321fb490dbf
msaner <msaner@fullsail.edu>
2013-10-16 21:05:06 -0700

    group one complete working on group 2 script

---
+++ Conditionals_worksheet/js/group2.js
@@ -0,0 +1,9 @@
+// Michael Saner
+// Oct. 16, 2013
+// Conditionals Worksheet
+
+//check login username and password
+
+var userName = prompt("Please create a User name:");
+var password = prompt("Please create a password:");
+
