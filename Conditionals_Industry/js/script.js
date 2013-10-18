2eeca198269f1abce9667adf29f3dddc8dd8749d
msaner <msaner@fullsail.edu>
2013-10-17 17:49:01 -0700

    typed out sentToA prompt wrong.  corrected took out second = sign

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -4,7 +4,7 @@
 
 // A/B split test - compare two email reults to see which one performed best.
 
-var sentToA = prompt = ("How many people did you send Email 'A' to?");
+var sentToA = prompt("How many people did you send Email 'A' to?");
 var clickThroughA = prompt("what was the click through rate for Email 'A'?");
 
 
