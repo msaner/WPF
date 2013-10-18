416fa3b3cf1ca6d02305a9fb6e2800acbafd5ba4
msaner <msaner@fullsail.edu>
2013-10-17 17:58:18 -0700

    changed the question for the clickThroughA and B to be more accurate

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -6,9 +6,12 @@
 
 //prompt user to enter info for Email A
 var sentToA = prompt("How many people did you send Email 'A' to?");
-var clickThroughA = prompt("What was the click through rate for Email 'A'?");
+var clickThroughA = prompt("How many people clicked on a link in Email 'A'?");
 
+//user enter info for email B and store in these variables
 var sentToB = prompt("How many people did you send Email 'B' to?");
-var clickThroughB = prompt("What was the click through rate for Email 'B'?");
+var clickThroughB = prompt("How many people clicked on a link in Email 'B'?");
+
+// 
 
 console.log(sentToB + clickThroughB); 
