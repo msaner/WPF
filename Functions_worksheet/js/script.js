bc2bdafbaafb416b76c5c6e42724c3818eef8db3
msaner <msaner@fullsail.edu>
2013-10-22 21:44:53 -0700

    used concatenation to add on "the circumference of the circle is" and then the result

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -21,4 +21,4 @@ function findCirc(dia, pi){ // the parameters will hold the values being passed 
 
 // console log is displaying the result of the function called findCirc
 // lets make this read like a scentence so it's clear what this number means
-console.log(answer);
+console.log("The circumference of the circle is " + answer);
