8539ac68092d6892fd3847b94c45f476b8ddc1be
msaner <msaner@fullsail.edu>
2013-10-22 21:26:38 -0700

    gave comments to the function line after adding parameters

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -11,6 +11,6 @@ var radius = 5;
 var answer = findCirc(2, 3.14); // 2 to find the diameter and 3.14 represents pi to find cirumference
 
 // this function will run the calculation to find our answer
-function findCirc(){
+function findCirc(dia, pi){ // the parameters will hold the values being passed on by the arguments above
 	
 }
