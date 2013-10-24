1e01979d6b53adc8c367063c64895d061321b34c
msaner <msaner@fullsail.edu>
2013-10-23 21:30:36 -0700

    added comment explaining the third variable

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -7,5 +7,7 @@
 
 // we'll store the user input for how many miles they will drive in this variable
 var miles = prompt("How many miles will you be driving?");
+// we'll store the value the user gives as the MPG their car has
 var carMpg = prompt("What is your vehicles MPG?");
+// next we'll ask the user to tell us if they want to calculate fuel cost 1-way or round trip and store that answer in the var 
 
