6e5d44da1d092d56324da92b51741f7d71e769d2
msaner <msaner@fullsail.edu>
2013-10-24 00:48:43 -0700

    added variable to hold the height in inches that will be converted

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -4,5 +4,6 @@
 
 var inToPx = prompt("Are you converting Inches to Pixels?  Enter 'yes' or 'no'.");
 var dpi = prompt("What is the DPI setting of the file you'll be creating?");
-var InWidth = prompt("Enter the width in Inches:"); 
+var InWidth = prompt("Enter the width in Inches:");
+var InHeight = prompt("Enter the height in Inches:"); 
 
