a683d2e55cc2ffe56b11e44700c20da135b438ec
msaner <msaner@fullsail.edu>
2013-10-24 00:53:53 -0700

    added if statement to run function if input is yes that the user wants to convert in to px.

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -9,3 +9,10 @@ var dpi = prompt("What is the DPI setting of the file you'll be creating?");
 var InWidth = prompt("Enter the width in Inches:");
 var InHeight = prompt("Enter the height in Inches:"); 
 
+if(inToPx === "yes"){
+	var convert = function(dpi, width, height){
+		var pixelw = dpi * width;
+		console.log(pixelw);
+	}}
+	
+	
