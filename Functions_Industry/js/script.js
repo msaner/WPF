c879d44a60d7df3d531265cca32bf0977214e1c4
msaner <msaner@fullsail.edu>
2013-10-24 01:44:21 -0700

    nothings working ….. starting over

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -2,23 +2,4 @@
 // 10-23-2013
 // Functions Industry
 
-// Inches to Pixels converter
-
-var inToPx = prompt("Are you converting Inches to Pixels?  Enter 'yes' or 'no'.");
-var dpi = prompt("What is the DPI setting of the file you'll be creating?");
-var inWidth = prompt("Enter the width in Inches:");
-var inHeight = prompt("Enter the height in Inches:"); 
-
-if(inToPx === "yes"){
-	var convert = function(dpi, inWidth, inHeight){
-		var pixelw = dpi * inWidth && dpi * inHeight;  
-		
-		return pixelw
-	}
-	
-	
-}else if(inToPx === "no"){
-	console.log("You're going to need a different converter.  This will only convert Inches to Pixels.");
-}
-	var pixels = convert(dpi, inWidth, inHeight);
-console.log(pixelw);
+// 
