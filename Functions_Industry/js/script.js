4c17623761d8cf586fe21bdd6015e045c4745130
msaner <msaner@fullsail.edu>
2013-10-24 01:04:06 -0700

    added second calculation to function

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -12,8 +12,12 @@ var inHeight = prompt("Enter the height in Inches:");
 if(inToPx === "yes"){
 	var convert = function(dpi, inWidth, inHeight){
 		var pixelw = dpi * inWidth;
+		var pixelh = dpi * inHeight;
+		
+		
 	}
 }else if(inToPx === "no"){
 	console.log("You're going to need a different converter.  This will only convert Inches to Pixels.");
 }
 	var pixels = convert(dpi, inWidth, inHeight);
+	console.log("pixels");
