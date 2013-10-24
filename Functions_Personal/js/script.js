ca964d73dca0c44bb87ce958533dd817d1359cdb
msaner <msaner@fullsail.edu>
2013-10-23 21:32:25 -0700

    added variable roundTrip the variable will determine how we calculate the fuel cost.

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -10,4 +10,5 @@ var miles = prompt("How many miles will you be driving?");
 // we'll store the value the user gives as the MPG their car has
 var carMpg = prompt("What is your vehicles MPG?");
 // next we'll ask the user to tell us if they want to calculate fuel cost 1-way or round trip and store that answer in the var 
+var roundTrip = prompt("Do you need to know the cost of gas for a round trip? Please enter yes or no.");
 
