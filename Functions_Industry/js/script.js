7ea3d96dbbad357eab3fec7fa294e502f204edbd
msaner <msaner@fullsail.edu>
2013-10-24 00:56:29 -0700

    fixed var naming and fixed the wrong var names being used in the arguments/parameters

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -6,13 +6,13 @@
 
 var inToPx = prompt("Are you converting Inches to Pixels?  Enter 'yes' or 'no'.");
 var dpi = prompt("What is the DPI setting of the file you'll be creating?");
-var InWidth = prompt("Enter the width in Inches:");
-var InHeight = prompt("Enter the height in Inches:"); 
+var inWidth = prompt("Enter the width in Inches:");
+var inHeight = prompt("Enter the height in Inches:"); 
 
 if(inToPx === "yes"){
-	var convert = function(dpi, width, height){
+	var convert = function(dpi, inWidth, inHeight){
 		var pixelw = dpi * width;
 		console.log(pixelw);
 	}}
 	
-	var pixels = convert(dpi, width, height);
+	var pixels = convert(dpi, inWidth, inHeight);
