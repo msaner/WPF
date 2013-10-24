3f46e9e6d5325f918e69d189ce27bffa5c8e0a55
msaner <msaner@fullsail.edu>
2013-10-24 00:49:36 -0700

    added the goal of the project in comments.  this is an Inches to Pixels converter

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -2,6 +2,8 @@
 // 10-23-2013
 // Functions Industry
 
+// Inches to Pixels converter
+
 var inToPx = prompt("Are you converting Inches to Pixels?  Enter 'yes' or 'no'.");
 var dpi = prompt("What is the DPI setting of the file you'll be creating?");
 var InWidth = prompt("Enter the width in Inches:");
