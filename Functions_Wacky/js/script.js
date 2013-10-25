a784e0bf1c7564c01360a9afa58e2c9b84e20196
msaner <msaner@fullsail.edu>
2013-10-24 19:10:26 -0700

    added a sweetTooth variable that will store a value from 1-10 that will tell us how much a person likes sweets.

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -9,4 +9,5 @@
 
 // we first need to find out how old the person is.  we will store this value in the age variable
 var age = prompt("How old are you?");
-
+// how big a sweet tooth a person has is the most important factor.  we'll store that in this variable
+var sweetTooth = prompt("On a scale of 1-10 how big is your sweet tooth?");
