0b2a30baf51ea4cbd10afa73835114e70f76b534
msaner <msaner@fullsail.edu>
2013-10-24 19:08:32 -0700

    added age variable and explained its purpose in comments

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -7,5 +7,6 @@
 //Just how many licks does it take to get to the center of a tootsie pop?  let's find out.
 // we'll start by getting some information from our user
 
-
+// we first need to find out how old the person is.  we will store this value in the age variable
+var age = prompt("How old are you?");
 
