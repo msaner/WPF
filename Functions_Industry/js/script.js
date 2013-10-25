8f3e4fbdb5f923fe4e4467a0a6275772b837adef
msaner <msaner@fullsail.edu>
2013-10-24 18:12:45 -0700

    fixed error… the first var was = Prompt made P lowercase so it would be valid

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -5,6 +5,6 @@
 // simple calculator that will multiply or divide for us
 
 // the answer will be stored here and tell the code to use either the multiply or divide function
-var mulOrDiv = Prompt("Do you want to multiply or divide?");
+var mulOrDiv = prompt("Do you want to multiply or divide?");
 
 var number1 = prompt("Enter a number to multiply:");
