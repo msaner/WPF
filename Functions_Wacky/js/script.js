586b8231539f633abbdb3d6c3fc141bc13e90bbc
msaner <msaner@fullsail.edu>
2013-10-24 19:15:10 -0700

    added the function call first will add the function above it so it's anonymous added comments to explain what's happening with all the elements

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -10,4 +10,10 @@
 // we first need to find out how old the person is.  we will store this value in the age variable
 var age = prompt("How old are you?");
 // how big a sweet tooth a person has is the most important factor.  we'll store that in this variable
-var sweetTooth = prompt("On a scale of 1-10 how big is your sweet tooth?");
+var sweetTooth = prompt("On a scale of 1(least)-10(most) how big is your sweet tooth?");
+
+// lets use an anonymous function here.  we have to define the function first.  the function call will come later
+
+
+// this calls the function named howMany.  the result of the function will be stored in variable licks.
+var licks = howMany(age, sweetTooth);
