c437c30ab23baa134723fb7c57ec65e2d313c81f
msaner <msaner@fullsail.edu>
2013-10-24 18:13:29 -0700

    added comment to explain the number1 variable

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -6,5 +6,6 @@
 
 // the answer will be stored here and tell the code to use either the multiply or divide function
 var mulOrDiv = prompt("Do you want to multiply or divide?");
-
+// user input will store here and be multiplied by the second and third numbers 
 var number1 = prompt("Enter a number to multiply:");
+
