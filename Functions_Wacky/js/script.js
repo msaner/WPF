c66b0002c3d17cbddf7607ec1078f690bf774f15
msaner <msaner@fullsail.edu>
2013-10-24 19:07:28 -0700

    added the puprose of the function and explanation of what the goal is to comments

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -2,3 +2,10 @@
 // 10-24-2013
 // Functions Wacky
 
+// How many licks?
+
+//Just how many licks does it take to get to the center of a tootsie pop?  let's find out.
+// we'll start by getting some information from our user
+
+
+
